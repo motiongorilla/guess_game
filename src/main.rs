6cba44c5033b75c79735ce8e03c8e29ec2db2a1a
motiongorilla <nize.afonya@gmail.com>
2024-08-15 19:02:42 +0100

    finished guess the number game in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number! The game!");
+    let number_to_guess = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess!");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Input a valid number");
+                continue;
+            }
+        };
+
+        match guess.cmp(&number_to_guess) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You guessed correct!");
+                break;
+            }
+        }
+    }
 }
